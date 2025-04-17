ee631d08fc865fedb67a65ef2cbe7260e66b803c
pranavgadham <pranavgadham@gmail.com>
2025-04-17 10:30:40 +0530

    plate schema update

--- src/feature/numberplate/numberPlate.schema.js
+++ src/feature/numberplate/numberPlate.schema.js
@@ -4,12 +4,6 @@ const numberPlateSchema = new mongoose.Schema({
     regNumber: {
         type: String,
         required: true,
-        validate: {
-            validator: function (value) {
-                return /^[A-Z]{2}\s?\d{1,2}\s?[A-Z]{1,2}\s?\d{4}$/.test(value);
-            },
-            message: props => `${props.value} is not a valid Indian number plate format!`
-        }
     },
     date: {
         type: Date,
